05400c077547d42df72da8235bd910a7ed390d87
Maksim Pasnitsenko <emexpas@outlook.com>
2024-08-12 14:31:44 +0300

    added images and passed some props to show 1 comp 3 times

---
+++ src/components/ImageDetail.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ImageDetail = ({ title }) => {
+  return <Text>Show image of {title}</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default ImageDetail;
