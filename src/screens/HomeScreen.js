0924f3c2d4c8bdfd39273a5ed7eac06335298cec
Maksim Pasnitsenko <emexpas@outlook.com>
2024-08-15 16:24:56 +0300

    ColorScreen and generate color func

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -21,6 +21,10 @@ const HomeScreen = ({ navigation }) => {
         title="Go to Counter Demo"
         onPress={() => navigation.navigate('Counter')}
       />
+      <Button
+        title="Go to Color Screen"
+        onPress={() => navigation.navigate('Color')}
+      />
       {/* <TouchableOpacity onPress={() => navigation.navigate('List')}>
         <Text>Go to List Demo</Text>
       </TouchableOpacity> */}
