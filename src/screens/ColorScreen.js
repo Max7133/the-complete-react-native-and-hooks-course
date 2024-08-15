444e2a37d59b00ffb72bdec1bec20c79dc996dd3
Maksim Pasnitsenko <emexpas@outlook.com>
2024-08-15 16:54:01 +0300

    showing and adding random colors in flatlist

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,15 +1,33 @@
-import React from 'react';
-import { View, StyleSheet, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, Button, FlatList } from 'react-native';
 
 const ColorScreen = () => {
+  const [colors, setColors] = useState([]);
+
   return (
     <View>
-      <Button title="Add a Color" />
-      <View
-        style={{
-          height: 100,
-          width: 100,
-          backgroundColor: randomRgb(),
+      <Button
+        title="Add a Color"
+        onPress={() => {
+          setColors([...colors, randomRgb()]);
+        }}
+      />
+      {/* data - array of pieces of data that I want to turn into a list*/}
+      {/* renderItem - a func that's is going to be called with every element inside the colors array*/}
+      {/* item - some Element (random rbg color) out of the 'colors' Array */}
+      <FlatList
+        keyExtractor={(item) => item}
+        data={colors}
+        renderItem={({ item }) => {
+          return (
+            <View
+              style={{
+                height: 100,
+                width: 100,
+                backgroundColor: item,
+              }}
+            />
+          );
         }}
       />
     </View>
