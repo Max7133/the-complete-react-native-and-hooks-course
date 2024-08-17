e59cd792da0f2482cc93fac80c7d92e584385f08
Maksim Pasnitsenko <emexpas@outlook.com>
2024-08-17 14:06:59 +0300

    renamed the variables with community convention names in useReducer()

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -6,24 +6,25 @@ import ColorCounter from './ColorCounter';
 const COLOR_INCREMENT = 15;
 
 // 1 arg - state === { red: 0, green: 0, blue: 0 }
-// 2nd Arg (action) === Obj that describes how it needs to change/update the State Obj === { colorToChange: 'red' || 'green' || 'blue', amount: 15 || -15 }
+// 2nd Arg (action) === Obj that describes how it needs to change/update the State Obj === { type: 'change_red' || 'change_green' || 'change_blue', payload: 15 || -15 }
 const reducer = (state, action) => {
-  switch (action.colorToChange) {
-    case 'red':
+  switch (action.type) {
+    case 'change_red':
       // Validation
-      return state.red + action.amount > 255 || state.red + action.amount < 0
+      return state.red + action.payload > 255 || state.red + action.payload < 0
         ? state // must return existing State Obj, otherwise it will set itself as 'undefined'
         : // take the current amount whatever the 'red' is and add in hovewer much I specified on that 'action' Obj
-          { ...state, red: state.red + action.amount }; // copy paste all the Values out of 'state', adding them to this New Obj and overwriting the New Obj (NO CHANGES MADE TO THE ORIGINAL 'state' OBJ)
-    case 'green':
-      return state.green + action.amount > 255 ||
-        state.green + action.amount < 0
+          { ...state, red: state.red + action.payload }; // copy paste all the Values out of 'state', adding them to this New Obj and overwriting the New Obj (NO CHANGES MADE TO THE ORIGINAL 'state' OBJ)
+    case 'change_green':
+      return state.green + action.payload > 255 ||
+        state.green + action.payload < 0
         ? state
-        : { ...state, green: state.green + action.amount };
-    case 'blue':
-      return state.blue + action.amount > 255 || state.blue + action.amount < 0
+        : { ...state, green: state.green + action.payload };
+    case 'change_blue':
+      return state.blue + action.payload > 255 ||
+        state.blue + action.payload < 0
         ? state
-        : { ...state, blue: state.blue + action.amount };
+        : { ...state, blue: state.blue + action.payload };
     default:
       return state;
   }
@@ -37,28 +38,28 @@ const SquareScreen = () => {
     <View>
       <ColorCounter
         onIncrease={
-          () => dispatch({ colorToChange: 'red', amount: COLOR_INCREMENT }) // passed an Obj (action Obj from 'reducer' with 'colorToChange' and 'amount')
+          () => dispatch({ type: 'change_red', payload: COLOR_INCREMENT }) // passed an Obj (action Obj from 'reducer' with 'type' and 'payload')
         }
         onDecrease={() =>
-          dispatch({ colorToChange: 'red', amount: -1 * COLOR_INCREMENT })
+          dispatch({ type: 'change_red', payload: -1 * COLOR_INCREMENT })
         }
         color="Red"
       />
       <ColorCounter
         onIncrease={() =>
-          dispatch({ colorToChange: 'green', amount: COLOR_INCREMENT })
+          dispatch({ type: 'change_green', payload: COLOR_INCREMENT })
         }
         onDecrease={() =>
-          dispatch({ colorToChange: 'green', amount: -1 * COLOR_INCREMENT })
+          dispatch({ type: 'change_green', payload: -1 * COLOR_INCREMENT })
         }
         color="Green"
       />
       <ColorCounter
         onIncrease={() =>
-          dispatch({ colorToChange: 'blue', amount: COLOR_INCREMENT })
+          dispatch({ type: 'change_blue', payload: COLOR_INCREMENT })
         }
         onDecrease={() =>
-          dispatch({ colorToChange: 'blue', amount: -1 * COLOR_INCREMENT })
+          dispatch({ type: 'change_blue', payload: -1 * COLOR_INCREMENT })
         }
         color="Blue"
       />
