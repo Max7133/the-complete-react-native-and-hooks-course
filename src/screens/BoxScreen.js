051f88faa4c83a2b56fbec50d2acc28ce45f659d
Maksim Pasnitsenko <emexpas@outlook.com>
2024-08-18 15:07:51 +0300

    alignSelf  property on Child Elements

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,6 +3,7 @@ import { Text, View, StyleSheet } from 'react-native';
 
 const BoxScreen = () => {
   // (always on parent element) alingItems: 'stretch', flexDirection: 'column', justifyContent: 'flex-start' by default
+  // adding "alingSelf" on Child - overwrites "alignItems" on the Parent Element
   return (
     <View style={styles.viewStyle}>
       <Text style={styles.textOneStyle}>Child 1</Text>
@@ -17,24 +18,23 @@ const styles = StyleSheet.create({
     borderWidth: 3,
     borderColor: 'black',
     height: 200,
+    alignItems: 'center', // Child 1 and 3 will center themselves, but Child 2 will remain on the right hand side, because it is set to alignSelf: 'flex-end'
     // flexDirection: 'row', // will now try to stretch vertically after applying 'height'
     //justifyContent: 'flex-start',
   },
   textOneStyle: {
     borderWidth: 3,
     borderColor: 'red',
-    flex: 4,
   },
   textTwoStyle: {
     borderWidth: 3,
     borderColor: 'red',
-    flex: 4,
+    alignSelf: 'flex-end',
     // flex: 1, // caused 'Child 2' to expand and take up as much vertical space as it can (if applied 'flex: 1' everywhere, then it will make all boxes equal size)
   },
   textThreeStyle: {
     borderWidth: 3,
     borderColor: 'red',
-    flex: 2,
   },
 });
 
