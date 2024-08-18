9c898797ef448a89a61373c34be0910577462a08
Maksim Pasnitsenko <emexpas@outlook.com>
2024-08-18 14:59:32 +0300

    Flex property on Child Elements

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -5,9 +5,9 @@ const BoxScreen = () => {
   // (always on parent element) alingItems: 'stretch', flexDirection: 'column', justifyContent: 'flex-start' by default
   return (
     <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>Child 1</Text>
-      <Text style={styles.textStyle}>Child 2</Text>
-      <Text style={styles.textStyle}>Child 3</Text>
+      <Text style={styles.textOneStyle}>Child 1</Text>
+      <Text style={styles.textTwoStyle}>Child 2</Text>
+      <Text style={styles.textThreeStyle}>Child 3</Text>
     </View>
   );
 };
@@ -16,13 +16,25 @@ const styles = StyleSheet.create({
   viewStyle: {
     borderWidth: 3,
     borderColor: 'black',
-    flexDirection: 'row', // will now try to stretch vertically after applying 'height'
     height: 200,
-    justifyContent: 'flex-start',
+    // flexDirection: 'row', // will now try to stretch vertically after applying 'height'
+    //justifyContent: 'flex-start',
   },
-  textStyle: {
+  textOneStyle: {
     borderWidth: 3,
     borderColor: 'red',
+    flex: 4,
+  },
+  textTwoStyle: {
+    borderWidth: 3,
+    borderColor: 'red',
+    flex: 4,
+    // flex: 1, // caused 'Child 2' to expand and take up as much vertical space as it can (if applied 'flex: 1' everywhere, then it will make all boxes equal size)
+  },
+  textThreeStyle: {
+    borderWidth: 3,
+    borderColor: 'red',
+    flex: 2,
   },
 });
 
