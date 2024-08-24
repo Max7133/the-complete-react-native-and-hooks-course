3a397273736a7951ee604e5b180100dd74c95033
Maksim Pasnitsenko <emexpas@outlook.com>
2024-08-24 16:20:31 +0300

    added state to search bar

--- food/src/components/SearchBar.js
+++ food/src/components/SearchBar.js
@@ -2,11 +2,21 @@ import React from 'react';
 import { View, TextInput, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
   return (
     <View style={styles.backgroundStyle}>
       <Feather name="search" style={styles.iconStyle} />
-      <TextInput style={styles.inputStyle} placeholder="Search" />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.inputStyle}
+        placeholder="Search"
+        value={term}
+        // onChangeText={(newTerm) => onTermChange(newTerm)}
+        onChangeText={onTermChange} // same as upper (reference to a function)
+        // onEndEditing={() => onTermSubmit()}
+        onEndEditing={onTermSubmit} // same as upper (reference to a function)
+      />
     </View>
   );
 };
