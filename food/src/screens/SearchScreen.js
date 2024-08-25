5292df62c013b144c2351646743e2d77cd8b56f9
Maksim Pasnitsenko <emexpas@outlook.com>
2024-08-25 18:56:56 +0300

    installed axios and fetched the data from yelp api

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,18 +1,34 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+
+  const searchApi = async () => {
+    const response = await yelp.get('/search', {
+      params: {
+        limit: 50,
+        term, // in JSON this called also 'term', therefore instead of 'term: term' I wrote just 'term'
+        location: 'helsinki',
+      },
+    });
+    setResults(response.data.businesses);
+  };
 
   return (
     <View>
       <SearchBar
         term={term}
-        onTermChange={(newTerm) => setTerm(newTerm)}
-        onTermSubmit={() => console.log('submitted')}
+        // onTermChange={(newTerm) => setTerm(newTerm)}
+        onTermChange={setTerm}
+        // onTermSubmit={() => searchApi()}
+        onTermSubmit={searchApi}
       />
       <Text>Search Screen</Text>
+      <Text>We have found {results.length} results</Text>
     </View>
   );
 };
