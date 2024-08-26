a736249319b01e4b62346137486b2deb6f5938e0
Maksim Pasnitsenko <emexpas@outlook.com>
2024-08-26 12:03:12 +0300

    implemented error handling and initial search on app load

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
@@ -6,17 +6,28 @@ import yelp from '../api/yelp';
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
-  const searchApi = async () => {
-    const response = await yelp.get('/search', {
-      params: {
-        limit: 50,
-        term, // in JSON this called also 'term', therefore instead of 'term: term' I wrote just 'term'
-        location: 'helsinki',
-      },
-    });
-    setResults(response.data.businesses);
+  const searchApi = async (searchTerm) => {
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          // term, // in JSON this called also 'term', therefore instead of 'term: term' I wrote just 'term'
+          term: searchTerm, // initial search when the app starts
+          location: 'helsinki',
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage('Something went wrong');
+    }
   };
+
+  // Call searchApi ONLY ONCE when component is first rendered.
+  useEffect(() => {
+    searchApi('pasta');
+  }, []);
 
   return (
     <View>
@@ -25,9 +36,10 @@ const SearchScreen = () => {
         // onTermChange={(newTerm) => setTerm(newTerm)}
         onTermChange={setTerm}
         // onTermSubmit={() => searchApi()}
-        onTermSubmit={searchApi}
+        // onTermSubmit={searchApi}
+        onTermSubmit={() => searchApi(term)}
       />
-      <Text>Search Screen</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
   );
