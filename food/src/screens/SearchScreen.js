c319c73bcae1e74f887811569240a4142f3e5f29
Maksim Pasnitsenko <emexpas@outlook.com>
2024-09-06 11:44:14 +0300

    grouped the results and rendered them on the FlatList

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -2,11 +2,20 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [searchApi, results, errorMessage] = useResults();
 
+  // price === '$' || '$$' || '$$$'
+  const filterResultsByPrice = (price) => {
+    return results.filter((result) => {
+      // for every 'result' inside of results Array it will ask if the
+      return result.price === price; // if this is true, it will return this inside of the new 'results' Array
+    });
+  };
+  console.log(results);
   return (
     <View>
       <SearchBar
@@ -19,6 +28,9 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
+      <ResultsList results={filterResultsByPrice('€')} title="Cost Effective" />
+      <ResultsList results={filterResultsByPrice('€€')} title="Bit Pricier" />
+      <ResultsList results={filterResultsByPrice('€€€')} title="Big Spender" />
     </View>
   );
 };
